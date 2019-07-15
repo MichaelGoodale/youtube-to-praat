d83f20859bfedde4f2618aedeecf3dde84dd31af
Michael Goodale <michael.goodale@mail.mcgill.ca>
2019-07-15 16:45:10 -0400

    Slight refactor

--- download.py
+++ download.py
@@ -12,11 +12,8 @@ def strip_bad_stuff(caption):
 TEMP_DIR = "output"
 TEXTGRID_DIR = "textgrids"
 ALIGNED_DIR = "aligned_textgrids"
-
-
 LANGUAGES = ["en"]
-
-ydl_opts = {
+YDL_OPTS = {
     'format': 'bestaudio/best',
     'writeautomaticsub': True,
     'outtmpl': TEMP_DIR+'/%(id)s.%(ext)s',
@@ -27,28 +24,37 @@ ydl_opts = {
     }]
 }
 
-#with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
+youtube_videos = ['https://www.youtube.com/watch?v=BaW_jenozKc']
+
+with youtube_dl.YoutubeDL(YDL_OPTS) as ydl:
+    ydl.download(youtube_videos)
 
 for subtitle in filter(lambda x: x.endswith(".vtt"), os.listdir(TEMP_DIR)):
-    
-    found_file = False
+    #Guess what language the file is
     for l in LANGUAGES:
-        audio = subtitle.replace(".{}.vtt".format(l), ".mp3")
-        if os.path.isfile(os.path.join(TEMP_DIR, audio)):
-            found_file = True
+        if subtitle.endswith(".{}.vtt".format(l)):
+            language = l
+            file_ending = ".{}.vtt".format(language)
             break
 
-    if not found_file:
+    #Check there's an audio file associated
+    audio = subtitle.replace(file_ending, ".mp3")
+    if not os.path.isfile(os.path.join(TEMP_DIR, audio)):
         print("Audio file {} is missing".format(audio))
-    
+
+    #Make sure the subtitles don't overlap as there are overlapping subtitles 
+    #in youtube's auto-generated subs
     non_overlapping_captions = []
     for cap in webvtt.read(os.path.join(TEMP_DIR, subtitle)):
         if len(non_overlapping_captions) == 0 or non_overlapping_captions[-1].end <= cap.start:
             non_overlapping_captions.append(strip_bad_stuff(cap))
 
+
+    #Create a textgrid with intervals of the subtitles
     tier = textgrid.IntervalTier("words")
     for cap in non_overlapping_captions:
         tier.add(cap.start_in_seconds, cap.end_in_seconds, cap.text)
 
-
+    tg = textgrid.TextGrid()
+    tg.append(tier)
+    tg.write(os.path.join(TEXTGRID_DIR, subtitle.replace(file_ending, ".TextGrid")))
